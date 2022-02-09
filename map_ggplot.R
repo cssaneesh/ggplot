bdd97ba4af7911514a442a7a549c5d7e9d7855d8
Saneesh <cssaneesh@gmail.com>
2022-02-09 22:52:31 +0100

    first commit ggplot map

--- map_ggplot.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-xlims <- c(-70, 290)
-ylims <- c(-55, 75)
-df_map <- map_data('world', wrap=xlims, ylim=ylims)
-ggplot(df_map, aes(long, lat, group=group, color='red'))+
-  geom_polygon()+
-  coord_map('gilbert', xlim = xlims, ylim = ylims)
